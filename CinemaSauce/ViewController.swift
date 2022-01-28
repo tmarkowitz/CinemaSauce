29a24a1ee58ea78c794160d74f0372d94cda7ecd
tmarkowitz <71396390+tmarkowitz@users.noreply.github.com>
2022-01-27 17:32:07 -0800

    Initial Commit

---
+++ CinemaSauce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinemaSauce
+//
+//  Created by ohadmin on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
