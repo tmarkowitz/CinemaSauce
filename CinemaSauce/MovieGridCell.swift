3e288e0815ee3f566118b420e9a52630c4b21fba
tmarkowitz <tmarkowitz5518@sdsu.edu>
2022-03-04 21:46:23 -0800

    part 2

---
+++ CinemaSauce/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  CinemaSauce
+//
+//  Created by ohadmin on 3/4/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- CinemaSauce/MoviesViewController.swift
+++ CinemaSauce/MoviesViewController.swift
@@ -20,8 +20,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.delegate = self
         tableView.rowHeight = 186
         // Do any additional setup after loading the view.
-        print("Hello")
-        
+       
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -63,5 +62,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for:cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MoviesDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath,animated:true)
+    }
 }
 
